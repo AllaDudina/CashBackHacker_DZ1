e7912b2bde80f0210b803c94e04101b3db03393c
Alla <thedreamtea@gmail.com>
2023-03-29 20:11:11 +1200

    Add TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldServiceIfAmountBelowBoundary() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldServiceIfAmountEqualBoundary() {
+        int actual = service.remain(1000);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
